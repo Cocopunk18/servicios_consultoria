3084291d6f639aa31bd9d52d6192c65a6aa3cfea
VIVIANA RESTREPO QUINTERO <lvrestrepo89570@umanizales.edu.co>
2023-03-03 21:15:47 -0500

    Se crean paquetes, index y modelo del proyecto

---
+++ src/models/serviciosconsul.models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const serviciosconsul_schema = mongoose.Schema ({
+    nit: {type: String, required: true, unique: true},
+    nombre: {type:String, required:true},
+    servicios: {type: Array, required:true},
+    precio: {type: parseFloat, required:true},
+    encargado: {type: String, required:true},
+    fecha_auditoria: {type: Date, required:true},
+    resultado_auditoria: {type: Object, required:true,
+        opinion_limpia: {type: String, required:true},
+        opinion_salvedades: {type: String, required:true},
+        opinion_adversa: {type: String, required:true},
+        opinion_denegada: {type: String, required:true},
+    }
+});
+
+module.exports= mongoose.model("ServiciosconsulCollection", serviciosconsul_schema);
